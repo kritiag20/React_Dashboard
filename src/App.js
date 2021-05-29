c03ec40f2bb305bd43d8170c65a324eef2e592ff
Kriti Agarwal <kritiagarwal2098@gmail.com>
2021-05-30 01:29:28 +0530

    Dashboard using React
    Signed-off-by: Kriti Agarwal <kritiagarwal2098@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import LineChart from './DashboardComp/LineChart';
+import RecentOrder from './DashboardComp/RecentOrder';
+import TopProducts from './DashboardComp/TopProducts';
+import ProductStatus from './DashboardComp/ProductStatus';
+import TotalRevenue from './DashboardComp/TotalRevenue';
+import TodayOrder from './DashboardComp/TodayOrder';
 
 function App() {
   return (
     <div className="App">
-      <div className="chart">
+      <TotalRevenue/>
+      <TodayOrder/>
+      {/* <ProductStatus/> */}
+      {/* <RecentOrder/> */}
+      {/* <div className="chart">
         <LineChart />
-      </div>
+      </div> */}
+      {/* <TopProducts/> */}
     </div>
   );
 }

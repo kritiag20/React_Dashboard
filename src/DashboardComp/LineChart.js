c4648feb81569d6631b8b6270df909f79fe4f7e8
Kriti Agarwal <kritiagarwal2098@gmail.com>
2021-05-31 03:29:02 +0530

    Line chart of Revenue
    Signed-off-by: Kriti Agarwal <kritiagarwal2098@gmail.com>

--- src/DashboardComp/LineChart.js
+++ src/DashboardComp/LineChart.js
@@ -1,11 +1,49 @@
 import React from 'react'
+import { Card, CardHeader, withStyles, makeStyles } from '@material-ui/core'
 import { Line } from 'react-chartjs-2'
 
 const datas = [49.734, 58.015, 42.542, 56.088, 58.694, 43.034, 47.487, 62.763, 44.234, 42.434, 58.348, 49.834]
-const LastYearData = [46.134, 47.015, 31.242, 45.088, 40.094, 31.334, 49.298, 33.187, 47.863, 34.034, 39.748, 32.834]
+const LastYearData = [46.134, 47.015, 31.242, 45.088, 40.094, 31.334, 33.090, 47.863, 34.034, 35.200, 39.748, 32.834]
 const LabelXaxis = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
+const CardHead = withStyles(() => ({
+    root: {
+        paddingBottom: 5,
+        marginBottom: -15,
+    },
+    title: {
+        fontSize: 18,
+        paddingLeft: 5,
+    },
+    content: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    subheader: {
+        border: 1,
+        padding: 2,
+        borderStyle: 'solid',
+        borderColor: '#818891',
+        width: 50,
+    }
+}))(CardHeader);
+
+
+const styling = makeStyles({
+    root: {
+        padding: 20,
+        margin: 15,
+        borderRadius: 15,
+        boxShadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+    },
+    body: {
+        width: 800,
+    },
+})
+
 function LineChart() {
+
+    const style = styling();
 
     const data = {
         labels: LabelXaxis,
@@ -71,13 +109,6 @@ function LineChart() {
             legend: {
                 align: 'start',
                 maxHeight: 90,
-                title: {
-                    display: true,
-                    text: 'Revenue',
-                    font: {
-                        size: 18
-                    }
-                },
                 labels: {
                     boxWidth: 10,
                     boxHeight: 10,
@@ -123,12 +154,12 @@ function LineChart() {
 
     return (
         <div>
-            <div className="year-cont">
-                <div className="year-dropdown">
-                    Year</div>
-
-            </div>
-            <Line data={data} options={options} />
+            <Card className={style.root}>
+                <CardHead title="Revenue" subheader="year" />
+                <div className="chart">
+                    <Line data={data} options={options} />
+                </div>
+            </Card>
         </div>
     )
 }
